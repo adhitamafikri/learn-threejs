51c76c5ec557b3d2b508da8c2fc9bc7d66508760
adhitamafikri <adhitamafikri@gmail.com>
2020-03-08 11:04:18 +0700

    chore: more updates on build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const DotenvWebpack = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public/dist'),
-    filename: '[name].[contentHash].js',
+    filename: '[name].[hash].js',
     chunkFilename: '[name].[contentHash].js'
   },
   module: {
@@ -24,13 +25,14 @@ module.exports = {
   },
   plugins: [
     new DotenvWebpack(),
+    new CleanWebpackPlugin(),
     new HTMLWebpackPlugin()
   ],
   devServer: {
     index: path.resolve(__dirname, 'public/dist/index.html'),
     contentBase: path.join(__dirname, 'public/dist'),
     hot: true,
-    port: 3004
+    port: 3004,
     writeToDisk: true,
     open: 'Google Chrome',
   }
